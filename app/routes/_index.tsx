a2c17156cae6286638171d77ad222d1099008b53
danilomsilva <danilomoreira1993@gmail.com>
2023-08-15 15:33:57 +0100

    added tailwind css

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,35 +1,12 @@
-import { json, type V2_MetaFunction } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+import type { V2_MetaFunction } from '@remix-run/node';
 
 export const meta: V2_MetaFunction = () => {
   return [
-    { title: 'New Remix App' },
-    { name: 'description', content: 'Welcome to Remix!' },
+    { title: 'Samsara' },
+    { name: 'Samsara Construtora', content: 'Samsara Construtora' },
   ];
 };
 
-export async function loader() {
-  try {
-    const response = await fetch(
-      'https://samsara-api-m38hl.ondigitalocean.app/api/operadores'
-    );
-    const data = await response.json();
-    return json({ data: data.data });
-  } catch (error) {}
-}
-
 export default function Index() {
-  const { data } = useLoaderData();
-
-  return (
-    <div>
-      {data.map((operador: any) => {
-        return (
-          <div key={operador.id}>
-            <div>{operador.attributes.nome_completo}</div>
-          </div>
-        );
-      })}
-    </div>
-  );
+  return <div className="text-3xl text-blue-500">Hello World!!</div>;
 }
