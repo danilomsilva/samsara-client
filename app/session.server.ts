333b354f8d31e91d54853492a8b586c168baf7c1
Danilo M. Silva <68205719+danilomsilva@users.noreply.github.com>
2023-08-22 17:46:22 +0100

    Update session.server.ts

--- app/session.server.ts
+++ app/session.server.ts
@@ -22,7 +22,7 @@ export const sessionStorage = createCookieSessionStorage({
     path: '/',
     sameSite: 'lax',
     secrets: [process.env.SESSION_SECRET],
-    secure: process.env.NODE_ENV === 'production',
+    secure: true,
   },
 });
 
