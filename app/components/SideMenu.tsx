685fe5d48bea648775c007dc3a8d998e569ed793
danilomsilva <danilomoreira1993@gmail.com>
2023-08-26 18:54:33 +0100

    added: collapsable side menu

--- app/components/SideMenu.tsx
+++ app/components/SideMenu.tsx
@@ -1,43 +1,65 @@
 import { Form, NavLink } from '@remix-run/react';
-import type { ReactNode } from 'react';
+import { useState, type ReactNode } from 'react';
+import DoubleLeftArrow from './icons/DoubleLeftArrow';
+import DoubleRightArrow from './icons/DoubleRightArrow';
+import clsx from 'clsx';
 
 export default function SideMenu({ children }: { children: ReactNode }) {
+  const [isOpen, setIsOpen] = useState(true);
+
   return (
     <div className="bg-grey-light h-screen flex">
-      <div className="bg-white h-screen w-[250px] px-6 py-8 gap-8 flex flex-col">
-        <div className="flex flex-col items-center gap-2">
-          <img src="/assets/logo.png" alt="logo" width={60} height={60} />
-          <h1 className="font-semibold">Olá, usuário!</h1>
+      <div
+        className={clsx(
+          ` ${
+            isOpen ? 'w-[250px]' : 'w-[50px]'
+          } bg-white h-screen px-6 py-8 gap-8 flex flex-col relative`
+        )}
+      >
+        {isOpen && (
+          <>
+            <div className="flex flex-col items-center gap-2">
+              <img src="/assets/logo.png" alt="logo" width={60} height={60} />
+              <h1 className="font-semibold">Olá, usuário!</h1>
+            </div>
+            <div className="border-grey-light border-t flex flex-col gap-2">
+              <h2 className="uppercase text-xs font-semibold mt-2">
+                Dashboard
+              </h2>
+              <div className="ml-3 flex flex-col gap-1">
+                <NavItem to="/dashboard" text="Dashboard" />
+              </div>
+            </div>
+            <div className="border-grey-light border-t flex flex-col gap-2 flex-1">
+              <h2 className="uppercase text-xs font-semibold mt-2">
+                Gerenciamento
+              </h2>
+              <div className="ml-3 flex flex-col gap-2 ">
+                <NavItem to="/obra" text="Obra" />
+                <NavItem to="/equipamento" text="Equipamento" />
+                <NavItem to="/manutencao" text="Manutenção" />
+                <NavItem to="/ordem-servico" text="Ordem de Serviço" />
+                <NavItem to="/operacao" text="Operação" />
+                <NavItem to="/boletim" text="Boletim" />
+                <NavItem to="/operador" text="Operador" />
+                <NavItem to="/usuario" text="Usuário" />
+              </div>
+            </div>
+            <div className="flex justify-between items-center border-grey-light border-t w-full ">
+              <Form action="/logout" method="post">
+                <button type="submit" className="pt-1 text-sm text-left">
+                  Sair
+                </button>
+              </Form>
+            </div>
+          </>
+        )}
+        <div
+          className="p-1 rounded-full bg-white hover:bg-grey-light cursor-pointer absolute bottom-11 -right-2"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? <DoubleLeftArrow /> : <DoubleRightArrow />}
         </div>
-        <div className="border-grey-light border-t flex flex-col gap-2">
-          <h2 className="uppercase text-xs font-semibold mt-2">Dashboard</h2>
-          <div className="ml-3 flex flex-col gap-1">
-            <NavItem to="/dashboard" text="Dashboard" />
-          </div>
-        </div>
-        <div className="border-grey-light border-t flex flex-col gap-2 flex-1">
-          <h2 className="uppercase text-xs font-semibold mt-2">
-            Gerenciamento
-          </h2>
-          <div className="ml-3 flex flex-col gap-2 ">
-            <NavItem to="/obra" text="Obra" />
-            <NavItem to="/equipamento" text="Equipamento" />
-            <NavItem to="/manutencao" text="Manutenção" />
-            <NavItem to="/ordem-servico" text="Ordem de Serviço" />
-            <NavItem to="/operacao" text="Operação" />
-            <NavItem to="/boletim" text="Boletim" />
-            <NavItem to="/operador" text="Operador" />
-            <NavItem to="/usuario" text="Usuário" />
-          </div>
-        </div>
-        <Form action="/logout" method="post">
-          <button
-            type="submit"
-            className="border-grey-light border-t w-full pt-1 text-sm text-left"
-          >
-            Sair
-          </button>
-        </Form>
       </div>
       <div className="w-full">
         <div className="border-t-blue border-t-8 w-full h-screen p-4">
---
+++ app/components/icons/DoubleLeftArrow.tsx
@@ -0,0 +1,18 @@
+export default function DoubleLeftArrow() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="h-4 w-4"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5"
+      />
+    </svg>
+  );
+}
