8dd06d4fc392f24d42df962dec6f43b7e173e045
danilomsilva <danilomoreira1993@gmail.com>
2023-08-27 15:13:07 +0100

    wip: modal

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,25 +1,27 @@
 import { type ReactNode } from 'react';
 import XIcon from './icons/XIcon';
-import LinkButton from './LinkButton';
-import PlusCircleIcon from './icons/PlusCircleIcon';
+import { Link } from '@remix-run/react';
 
 type PropTypes = {
+  title: string;
   children: ReactNode;
+  footerActions: ReactNode;
 };
 
-export default function Modal({ children }: PropTypes) {
+export default function Modal({ title, children, footerActions }: PropTypes) {
   return (
-    <div className="bg-black/30 absolute top-0 left-0 w-full h-screen flex justify-center items-center">
+    // TODO: find a way to close modal by clicking on overlay making sure it will navigate back to previous route and not to initial route
+    <div className="bg-black/30 absolute top-0 left-0 w-full h-screen flex justify-center items-center z-10">
       <div className="bg-grey-light rounded-lg border-t-8 border-t-blue w-[500px]">
         <div className="flex border-b px-8 p-6 justify-between w-full border items-center border-b-grey/50 border-x-0 border-t-0">
-          <h1 className="font-bold text-2xl text-blue">Adicionar Obra</h1>
-          <XIcon className="h-6 w-6 text-blue stroke-2" />
+          <h1 className="font-bold text-xl text-blue">{title}</h1>
+          <Link to=".." className="hover:bg-white/50 rounded-lg cursor-pointer">
+            <XIcon className="h-6 w-6 text-blue stroke-2 z-50" />
+          </Link>
         </div>
-        <div className="bg-white h-80 p-8">{children}</div>
-        <div className="border-b-grey/50 border-x-0 border-b-0 border-t h-16 flex items-center px-4 justify-end">
-          <LinkButton to="./new" className="bg-blue" icon={<PlusCircleIcon />}>
-            Adicionar
-          </LinkButton>
+        <div className="h-80 p-8">{children}</div>
+        <div className="border-t-grey/50 border-x-0 border-b-0 border-t h-16 flex items-center px-4 justify-end">
+          {footerActions}
         </div>
       </div>
     </div>
--- app/routes/usuario.new.tsx
+++ app/routes/usuario.new.tsx
@@ -1,5 +1,18 @@
+import LinkButton from '~/components/LinkButton';
 import Modal from '~/components/Modal';
+import PlusCircleIcon from '~/components/icons/PlusCircleIcon';
 
 export default function NewUsuario() {
-  return <Modal>some content</Modal>;
+  return (
+    <Modal
+      title="Adicionar Usuário"
+      footerActions={
+        <LinkButton to="./new" className="bg-blue" icon={<PlusCircleIcon />}>
+          Adicionar
+        </LinkButton>
+      }
+    >
+      some content
+    </Modal>
+  );
 }
