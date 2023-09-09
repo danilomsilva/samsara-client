8523bc45c8ef24e7f759702f270c1437988dcd49
danilomsilva <danilomoreira1993@gmail.com>
2023-09-09 11:13:48 +0100

    fix: modal minor improvement

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -44,13 +44,13 @@ export default function Modal({
   };
   return (
     // TODO: find a way to close modal by clicking on overlay making sure it will navigate back to previous route and not to initial route
-    <div className="bg-black/30 absolute top-0 left-0 w-full h-screen flex justify-center items-center z-10">
+    <div className="bg-black/30 absolute top-0 left-0 w-full h-screen flex justify-center items-center">
       <div
         className={`${
           variant && variantStyle[variant]?.borderTop
         } bg-grey-light rounded-lg border-t-8 w-[500px]`}
       >
-        <div className="flex border-b px-8 p-6 justify-between w-full border items-center border-b-grey/50 border-x-0 border-t-0">
+        <div className="flex border-b p-6 justify-between w-full border items-center border-b-grey/50 border-x-0 border-t-0">
           <h1
             className={`${
               variant && variantStyle[variant]?.title
@@ -83,8 +83,8 @@ export default function Modal({
           )}
         </div>
         <Form method="post">
-          <div className="p-8 gap-2 flex flex-col">{content}</div>
-          <div className="border-t-grey/50 border-x-0 border-b-0 border-t h-16 flex items-center px-4 justify-end">
+          <div className="p-8 px-6 gap-2 flex flex-col">{content}</div>
+          <div className="border-t-grey/50 border-x-0 border-b-0 border-t h-16 flex items-center px-6 justify-end">
             {footerActions}
           </div>
         </Form>
