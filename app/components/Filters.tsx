9de4465fbffec2753d6eb432b7a2c975fc34f2d6
Danilo <daanilomsilvaa@gmail.com>
2024-02-27 16:57:55 +0000

    WIP: great progress with filters and url changes

--- app/components/Filters.tsx
+++ app/components/Filters.tsx
@@ -22,11 +22,40 @@ export default function Filters({ column }: PropTypes) {
     timeoutId = setTimeout(handleFilterChange, 2000);
   };
 
+  // add or update the filter in the URL
   const handleFilterChange = () => {
     const newSearchParams = new URLSearchParams(searchParams);
-
-    newSearchParams.set('filter', `(${column}~'${filterValue}')`);
-    navigate(`${location.pathname}?${newSearchParams.toString()}`);
+    const currentFilter = newSearchParams.get('filter');
+    if (currentFilter?.includes(column)) {
+      let splitFilter = currentFilter?.split('&&');
+      splitFilter.forEach((element, index) => {
+        if (element.includes(column)) {
+          // replace the value for the column from (column~'value') to (column~'newValue')
+          const valueStartIndex = element.indexOf(`'`) + 1;
+          const valueEndIndex = element.lastIndexOf(`'`);
+          const newValue = `${filterValue}`;
+          const newElement =
+            element.substring(0, valueStartIndex) +
+            newValue +
+            element.substring(valueEndIndex);
+          splitFilter[index] = newElement;
+          const reJoined = splitFilter.join('&&');
+          newSearchParams.set('filter', `${reJoined}`);
+          navigate(`${location.pathname}?${newSearchParams.toString()}`);
+        }
+      });
+    } else {
+      if (!currentFilter) {
+        newSearchParams.set('filter', `(${column}~'${filterValue}')`);
+        navigate(`${location.pathname}?${newSearchParams.toString()}`);
+      } else {
+        let splitFilter = currentFilter?.split('&&');
+        splitFilter?.push(`(${column}~'${filterValue}')`);
+        const reJoined = splitFilter?.join('&&');
+        newSearchParams.set('filter', `${reJoined}`);
+        navigate(`${location.pathname}?${newSearchParams.toString()}`);
+      }
+    }
   };
 
   const handleFilterOnChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -40,7 +69,6 @@ export default function Filters({ column }: PropTypes) {
         name={column}
         value={filterValue}
         onChange={handleFilterOnChange}
-        onKeyUp={handleKeyUp}
         className="w-full p-1 px-2 border border-grey/30 rounded-md outline-blue"
       />
     </div>
