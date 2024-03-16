99346dd764205714ff94b68cc17a6206f8c9a5cc
danilomsilva <daanilomsilvaa@gmail.com>
2024-03-16 22:18:48 +0000

    FIX: file list loooooong names

--- app/components/FileList.tsx
+++ app/components/FileList.tsx
@@ -34,31 +34,36 @@ export default function FileList({
   return (
     <div className="text-sm">
       {files.length > 0 && <label>Anexos</label>}
-      <div className="flex flex-wrap gap-x-4 gap-y-1">
+      <div className="flex flex-wrap gap-x-4 gap-y-1 max-w-[280px] overflow-hidden">
         {files.map((item: FileTypes) => {
           const splitName = item?.name?.split('.');
           return (
-            <div key={item?.id} className="flex gap-2 items-center over">
-              <DocumentIcon className="h-4 w-4" />
+            <div key={item?.id} className="flex gap-2 items-center truncate">
+              <div>
+                <DocumentIcon className="h-4 w-4" />
+              </div>
               <div className="truncate overflow-hidden flex">
                 <a
                   href={`http://159.223.244.247/api/files/${item.collectionId}/${item.id}/${item.file}`}
                   target="_blank"
                   rel="noreferrer"
                   download={true}
+                  className="truncate hover:text-blue"
                 >
                   {splitName[0]}
                 </a>
                 <div>{`.${splitName[1]}`}</div>
               </div>
-              {isRemovingFile && removingFile === item.id ? (
-                <SpinnerIcon className="h-3 w-3" />
-              ) : (
-                <MinusCircleIcon
-                  className="h-4 w-4 text-grey hover:text-red cursor-pointer"
-                  onClick={() => handleRemoveFile(item)}
-                />
-              )}
+              <div>
+                {isRemovingFile && removingFile === item.id ? (
+                  <SpinnerIcon className="h-3 w-3" />
+                ) : (
+                  <MinusCircleIcon
+                    className="h-4 w-4 text-grey hover:text-red cursor-pointer"
+                    onClick={() => handleRemoveFile(item)}
+                  />
+                )}
+              </div>
             </div>
           );
         })}
